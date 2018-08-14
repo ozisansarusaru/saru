40e331b8b619f884f9d522be4db20b79d46ca20d
DA\nwuser <noppo.kappa@gmail.com>
2018-08-15 00:24:42 +0900

    【背景】ジェネレータ設置

---
+++ joubutu/Assets/WORK/ozisan/Scripts/Generator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+
+    /// <summary> 生成する背景</summary>
+    [SerializeField]
+    private GameObject m_BackGround;
+
+    IEnumerator Generate()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3f);
+            Instantiate(m_BackGround,transform.position,Quaternion.identity);
+        }
+    }
+
+    void Start(){
+        StartCoroutine(Generate());
+    }
+}
