34b83a438cc7bc33675519c7525315019b47cc61
DA\nwuser <noppo.kappa@gmail.com>
2018-09-02 04:13:59 +0900

    [おじさん]スタート時の処理書いてみた

---
+++ joubutu/Assets/WORK/ozisan/Scripts/GameManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameState
+{
+    Start,
+    Prepare,
+    Playing,
+    End
+}
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance;
+
+    // 現在の状態
+    private GameState currentGameState;
+
+
+    // 例
+
+    public Text label;
+    public Button button;
+
+    private Text m_text;
+
+    void Awake()
+    {
+        Instance = this;
+        SetCurrentState(GameState.Start);
+    }
+
+
+    // 外からこのメソッドを使って状態を変更
+    public void SetCurrentState(GameState state)
+    {
+        currentGameState = state;
+        OnGameStateChanged(currentGameState);
+    }
+
+    // 状態が変わったら何をするか
+    void OnGameStateChanged(GameState state)
+    {
+        switch (state)
+        {
+            case GameState.Start:
+                StartAction();
+                break;
+            case GameState.Prepare:
+                StartCoroutine(PrepareCoroutine());
+                break;
+            case GameState.Playing:
+                PlayingAction();
+                break;
+            case GameState.End:
+                EndAction();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Startになったときの処理
+    void StartAction()
+    {
+    }
+
+    // Prepareになったときの処理
+    IEnumerator PrepareCoroutine()
+    {
+        label.text = "3";
+        yield return new WaitForSeconds(1);
+        label.text = "2";
+        yield return new WaitForSeconds(1);
+        label.text = "1";
+        yield return new WaitForSeconds(1);
+        label.text = "";
+        SetCurrentState(GameState.Playing);
+    }
+    // Playingになったときの処理
+    void PlayingAction()
+    {
+        label.text = "ゲーム中";
+    }
+    // Endになったときの処理
+    void EndAction()
+    {
+    }
+}
---
+++ joubutu/Assets/WORK/ozisan/Scripts/Gomi/StartCountText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartCountText : MonoBehaviour {
+
+    [SerializeField]
+    private Text m_text;
+
+    IEnumerator CountTest()
+    {
+        m_text.text = "さん！";
+        yield return new WaitForSeconds(1);
+        m_text.text = "にい！";
+        yield return new WaitForSeconds(1);
+        m_text.text = "いち！";
+        yield return new WaitForSeconds(1);
+        m_text.text = "START!";
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        //m_text.enabled = false;
+        StartCoroutine(CountTest());
+    }
+}
