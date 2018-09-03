63efe86770d22632b4e8f63e0482e33b48fb24f2
DA\nwuser <goroumakisimamu810@gmail.com>
2018-09-03 15:29:49 +0900

    ゲームクリアまでの時間を追加したぜよ

---
+++ joubutu/Assets/WORK/sura/Scripts/StageTime.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageTime : MonoBehaviour {
+
+    [SerializeField]
+    private int maxTime;
+    [SerializeField]
+    private int nowTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        //初期化しよう
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //制限時間をあれする
+        Time();
+    }
+
+    void Init()
+    {
+        //nowTimeを初期化
+        nowTime = 0;
+    }
+
+    void Time()
+    {
+        //maxTimeまでカウントしような
+        if (maxTime >= nowTime)
+        {
+            nowTime++;
+        }
+        else
+        {
+            //生き残ったな
+            FadeScene();
+            nowTime = 0;
+        }
+    }
+
+    public void FadeScene()
+    {
+        FadeManager.Instance.LoadScene("Clear", 1.0f);
+    }
+}
